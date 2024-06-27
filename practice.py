e70ced20ef3fb94d27f8c517ea22b036af7c2022
jeevasap <jeevaa@strikeaprice.com>
2024-06-27 11:21:46 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,47 @@
+"""fruits = ["apple", "banana", "cherry"]
+print(type(fruits))
+for item in fruits:
+    print(item)
+ints = [1,2,3,4,5,6,7,8,9,10]
+total=0
+for x in ints:
+    total = total+x
+print(total)
+
+if 5 > 5:
+   print("Five is greater than two!")
+elif 5 == 5:
+   print("Its equal")
+else:
+   print("five is lesser than two!")"""
+
+def calculate():
+   return None
+   i = 10
+   print(1)
+   if True:
+      print(True)
+   print("out")
+
+
+
+# print("hello")
+
+   # return None
+
+def calc():
+   pass
+
+c = calculate()
+c1 = calc()
+print(c)
+print(c1)
+
+# public static void main(String args[]){
+
+# } 
+
+
+# for(i=0; i<=5; i++){
+
+# }
